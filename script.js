a88d60b959095a6df737997066faf7cbaa6d0439
Faridpro1 <94738065+Faridpro1@users.noreply.github.com>
2022-08-12 21:19:11 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+
+/*let age = 3;
+
+if(age >=18){
+    console.log("You can driver");
+}
+else if( age <18){
+    console.log(`Masin surmek ucun ${18 - age}`)
+}
+
+ if(age>  150){
+    console.log("Zehmet olmasa ededi duzgun daxil edin")
+}
+
+let a = 15;
+let b = 13;
+
+if(a > b){
+    console.log(`${a} , ${b} boyukdur`)
+} else{
+    console.log(`${a} , ${b} kickdir`)
+}
+
+
+let num = 3;
+
+if(num % 2 ==0){
+    console.log(`${num} cutdur`);
+} else {
+    console.log(`${num} tekdir`)
+}
+
+
+let qiymet = 89;
+
+ if(qiymet > 80){
+    console.log("A")
+}
+ else if(qiymet > 70){
+    console.log("B")
+}
+ else if(qiymet > 60){
+    console.log("C")
+}
+else if(qiymet > 50){
+    console.log("D")
+}
+ else if(qiymet > 0){
+    console.log("A")
+} 
+
+let day = prompt("Enter mounth")
+
+switch (day) {
+    case 'January':
+        alert("Jaunary has 31 days")
+        break;
+    case 'February':
+        alert("February has 28 days")
+        break;
+    case 'March':
+        alert("March has 31 dayas")
+        break;
+    case 'April':
+        alert("April has 30 dayas")
+        break;
+    case 'May':
+        alert("May has 31 dayas")
+        break;
+    case 'June':
+        alert("June has 30 days")
+        break;
+    case 'July':
+        alert("July has 31 days")
+        break;
+    case 'August':
+        alert("August has 30 days")
+        break;
+    case 'August':
+        alert("August has 31 days")
+        break;
+    case 'September':
+        alert("September has 30 days")
+        break;
+    case 'October':
+        alert("July has 31 days")
+        break;
+    case 'November':
+        alert("July has 30 days")
+        break;
+        case 'December':
+        alert("July has 31 days")
+        break;
+}
+*/
